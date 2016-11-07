74d7a477559e386d8f8ff6467f2798dcf1a0de81
Claudinei B. Junior <claudineibjr@hotmail.com>
2016-11-07 19:39:19 -0200

    Implementação da criação da tabela - Parte 1 Melhoria no método preencheTabela Criação do método preencheTabela_Aux Necessário ajustar o método transformaFuncao

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 function calcSimplex(){
 
 	// Definição da função que há de ser maximizada
-	var funcaoMaxZ = " + 3[x1] + 5[x2]";	// [TO DO]
+	var funcaoMaxZ = " {+} <3>[x1] {+} <5>[x2]";	// [TO DO]
 	// Função responsável por identificar as variáveis
 	var variaveis = criaVariaveis(funcaoMaxZ);
 
@@ -21,12 +21,11 @@ function calcSimplex(){
 
 	// Criação da tabela que será usada
 	var tabela = newMatriz(1, 2 + variaveis.length + variaveisFolgaExcesso.length + 1);
-	preencheTabela(tabela, restricoes, variaveis.length, variaveisFolgaExcesso.length);
+	preencheTabela(tabela, restricoes, variaveis, variaveisFolgaExcesso, funcaoObjetivoZ);
 
 }
-function preencheTabela(tabela, restricoes, variaveis, variaveisFolgaExcesso){
+function preencheTabela(tabela, restricoes, variaveis, variaveisFolgaExcesso, maxZ){
 	/*	
-
 		Colocar as restrições em cada linha;
 		Nas primeiras colunas (Base) escrever o nome dos elementos que estão na base; 
 		Na ultima coluna (b), escrever o valor dos elementos da base, inicialmente adicionando os valores da restrições; 
@@ -46,19 +45,49 @@ function preencheTabela(tabela, restricoes, variaveis, variaveisFolgaExcesso){
 			4ª		Z 		-3	-5	0	0	0	0
 	*/
 
-	var variaveis = new Array();
-
-	for (var i = 0; i < restricoes.length +1; i++ ){
+	for (var i = 0; i < restricoes.length + 1; i++ ){
 		var linha = i+1;
 		var base = (i == restricoes.length ? "Z" : "f" + (i+1));
 		var b = (i == restricoes.length ? 0 : restricoes[i][2]);
+		var variavel = new Array();
+		var variavelFolgaExcesso = new Array();		
 		
-		for (var j = 0; j < variaveis; j++){
-			
+		for (var j = 0; j < variaveis.length; j++){
+			//variavel.push(preencheTabela_Aux(variaveis[j], restricoes[i][0]));
+			variavel.push((i == restricoes.length ? preencheTabela_Aux(variaveis[j], maxZ, true) : preencheTabela_Aux(variaveis[j], restricoes[i][0], false)));
+		}
+		for (var j = 0; j < variaveisFolgaExcesso.length; j++){
+			//variavelFolgaExcesso.push(preencheTabela_Aux(variaveisFolgaExcesso[j], restricoes[i][0]));
+			variavelFolgaExcesso.push((i == restricoes.length ? preencheTabela_Aux(variaveisFolgaExcesso[j], maxZ, true) : preencheTabela_Aux(variaveisFolgaExcesso[j], restricoes[i][0], false)));
+		}
+		alert("Linha: " + linha + "\nBase: " + base + "\n" + showMatriz(variavel, false) + "\n" + showMatriz(variavelFolgaExcesso, false) + "B: " + b);
+	}
+}
+function preencheTabela_Aux(variavel, restricao, inverteSinal){
+	var iAux, resultado, iAux2;
+	iAux = 0;
+	iAux2 = 0;
+
+	resultado = 0;
+
+	for (var i = 0; i < restricao.length; i++){
+		if (restricao.substring(i, i+1) == "[")
+			iAux = i;
+		if (restricao.substring(i, i+1) == "]"){
+			var varAux;
+			varAux = restricao.substring(i+1, iAux);
+			varAux = varAux.replace("[", "");	varAux = varAux.replace("]", "");
+
+			if (varAux == variavel){
+				resultado = restricao.substring(iAux, iAux - i)
+				resultado = resultado.substring(resultado.lastIndexOf("<"));
+				resultado = resultado.replace("<", "");	resultado = resultado.replace(">", "");
+			}
 		}
 
 	}
-
+	return resultado;
+	//alert(variavel + "\n\n" + restricao);
 }
 function criaVariaveis(funcao){
 
@@ -117,19 +146,19 @@ function transformaFuncao(funcao){
 }
 function criaRestricoes(res){
 	// [TO DO]
-	res.push(["1[x1]", "<=", 4]);
-	res.push(["1[x2]", "<=", 6]);
-	res.push(["3[x1] + 2[x2]", "<=", 18]);
+	res.push(["{+} <1>[x1]", "<=", 4]);
+	res.push(["{+} <1>[x2]", "<=", 6]);
+	res.push(["{+} <3>[x1] {+} <2>[x2]", "<=", 18]);
 }
 function criaVariaveisFolgaExcesso(varFE, res){
 	for (var i = 0; i < res.length; i++)
-		varFE.push(0);
+		varFE.push("f" + (i+1));
 }
 function insereVariaveisFolga(res, varFE){
 	for (var i = 0; i < res.length; i++){
 		if (res[i][1] == "<=")
-			res[i][0] = res[i][0] + " + [f" + (i+1) + "]";
+			res[i][0] = res[i][0] + " {+} <1>[f" + (i+1) + "]";
 		else
-			res[i][0] = res[i][0] + " - [f" + (i+1) + "]";
+			res[i][0] = res[i][0] + " {-} <1>[f" + (i+1) + "]";
 	}
 }
